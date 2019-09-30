bc397613a406c8ccf87024931c6a1875232c4756
parsalotfy <parsalotfy@outlook.com>
2019-09-30 22:01:13 +0330

    Register the Repository Service.

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -16,6 +17,7 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
 
