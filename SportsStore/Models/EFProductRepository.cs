4d080cb05d02100b56528091db9b0fbf33a65b37
parsalotfy <parsalotfy@outlook.com>
2019-10-03 12:56:59 +0330

    Add EFProductRepository class.

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private SportsStoreContext context;
+        public EFProductRepository(SportsStoreContext sportsStoreContext)
+        {
+            context = sportsStoreContext;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
