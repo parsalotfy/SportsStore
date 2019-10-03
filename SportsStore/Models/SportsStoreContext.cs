35c2328b173e79f5c054c9f2ad95908d2ceb2d6a
parsalotfy <parsalotfy@outlook.com>
2019-10-03 12:52:55 +0330

    1) Delete duplicate Product.cs 2) Move SportsStoreContext to Models folder. 3) Fix SportsStoreContext.

---
+++ SportsStore/Models/SportsStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class SportsStoreContext : DbContext
+    {
+        public SportsStoreContext(DbContextOptions<SportsStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
